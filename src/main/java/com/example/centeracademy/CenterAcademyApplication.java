0e5ecb0d22425e4fc45d862d3433efe9c489300b
mahmoudmatar <mahmoudmatar49@gmail.com>
2023-08-12 17:22:07 -0700

    Initial commit

---
+++ src/main/java/com/example/centeracademy/CenterAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.example.centeracademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenterAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenterAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/centeracademy/CenterAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centeracademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterAcademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
